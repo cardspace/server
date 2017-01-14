63c1dfe93a6394fbafe0879c66d7f90e7becc870
William Paul Moore <wipmoore@outlook.com>
2017-01-14 21:08:37 +0000

    Remove unique index from card title

--- models/card.js
+++ models/card.js
@@ -2,7 +2,7 @@ const modelBuilder = require( './model-builder' );
 const mongoose = require( 'mongoose' );
 
 const cardSchema = new mongoose.Schema({
-    title: { type: String, required: true, unique: true },
+    title: { type: String, required: true },
     url: String,
     description: { type: String, required: true },
     createdBy: { type: mongoose.Schema.Types.ObjectId, index: true }, 
