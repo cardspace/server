7d46b9bdaa9ba8bf55b16acb6c248279c09249b8
William Paul Moore <wipmoore@outlook.com>
2017-01-16 07:51:04 +0000

    Add version number

--- resources/index.js
+++ resources/index.js
@@ -1,11 +1,15 @@
 const express = require( 'express' );
 const router = express.Router();
 
-router.use( '/v1/cards', require( './cards' )  );
+router.use( '/v1/cards', require( './cards' ) );
 router.use( '/v1/card', require( './card' ) );
 
 router.get( '/', ( req, res ) => {
   res.send( 'cardspace' );  
-})
+});
+
+router.get( '/version', ( req, res ) => {
+  res.send( '0.1.0' );
+});
 
 module.exports = router;
